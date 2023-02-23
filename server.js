75ab31c3c13a413a54a74a464cdbdab33f6ed0d8
anabelaSantos <anabelamsantos@gmail.com>
2023-02-23 22:21:36 +0000

    exercise 1 complete

--- server.js
+++ server.js
@@ -1,52 +1,55 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
 // List of Users
-let users = [
-    {
-        name: "User 1",
-        age: 30
-    },
-    {
-        name: "User 2",
-        age: 45
-    },
-    {
-        name: "User 3",
-        age: 27
-    },
-    {
-        name: "User 4",
-        age: 22
-    }
-]
+// let users = [
+//   {
+//     name: "User 1",
+//     age: 30,
+//   },
+//   {
+//     name: "User 2",
+//     age: 45,
+//   },
+//   {
+//     name: "User 3",
+//     age: 27,
+//   },
+//   {
+//     name: "User 4",
+//     age: 22,
+//   },
+// ];
 
 // List of Fruits
-let fruits = [
-    {
-        name: "Apple",
-        color: "Red"
-    },
-    {
-        name: "Banana",
-        color: "Yellow"
-    },
-    {
-        name: "Kiwi",
-        color: "Green"
-    },
-    {
-        name: "Grape",
-        color: "Purple"
-    },
-]
+// let fruits = [
+//   {
+//     name: "Apple",
+//     color: "Red",
+//   },
+//   {
+//     name: "Banana",
+//     color: "Yellow",
+//   },
+//   {
+//     name: "Kiwi",
+//     color: "Green",
+//   },
+//   {
+//     name: "Grape",
+//     color: "Purple",
+//   },
+// ];
 
 // Express Routes
 
+const usersRouter = require("./routers/users");
+app.use("/users", usersRouter);
 
-
+const fruitsRouter = require("./routers/fruits");
+app.use("/fruits", fruitsRouter);
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
